4b4bef4bd9840a36d2d86aab446f9291e5a78225
Paulo Carvalho <paulovcarvalhojr@gmail.com>
2026-03-06 19:31:42 -0300

    primeiro commit

---
+++ Primeiro_Programa.py
@@ -0,0 +1,9 @@
+# Sistema bancário - Módulo 1
+
+nome = input('Digite seu nome: ')
+cpf = input('Digite seu CPF: ')
+
+saldo = 1500.70
+print(f'Bem vindo, {nome}')
+print(f'CPF: {cpf}')
+print(f'Saldo atual: R${saldo:.2f}')
